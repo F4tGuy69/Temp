339abdc673a4dfdbf45cabe3eb7dd3beb278d134
Zaki <sangamzaki@gmail.com>
2023-09-30 08:09:01 +0530

    Version yay

---
+++ Temp.py
@@ -0,0 +1,1 @@
+print("If u are reading this imagine how useless u are to raed files of another person")
---
+++ hello.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter a value:");
+    scanf("%d", &a);
+    printf("The value is:%d", a);
+    return 0;
+}
